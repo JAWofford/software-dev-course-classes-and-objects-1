7bbafe59af9444656f6a028d1d36f9e1929a9319
JWofford <118766665+JAWofford@users.noreply.github.com>
2026-05-04 13:55:51 -0500

    created methods

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,10 +2,29 @@ package org.example;
 
 public class Calculator {
     // Add a static function called add that takes two int parameters and returns their sum (as an int)
+    public static int add(int a, int b){
+        return a + b;
+    }
 
     // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
-
+    public static int subtract(int a, int b){
+        return a - b;
+    }
     // Add a static function called multiply that takes two int parameters and returns their product (as an int)
-
+    public static int multiply(int a, int b){
+        return a * b;
+    }
     // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+    //testing locally.
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(1, 2));
+        System.out.println(Calculator.subtract(1, 2));
+        System.out.println(Calculator.multiply(1, 2));
+        System.out.println(Calculator.divide(8, 2));
+    }
 }
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -6,11 +6,43 @@ public class StringUtilities {
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
 
+    public static boolean shortString(String word){
+        return word !=null && word.length() < 5;
+    }
+
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static char firstLetter(String word){
+        return word.charAt(0);
+        //I wanted to do validation here but not sure about throwing errors in java yet.
+    }
 
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
-
+    public static String censorAsparagus (String phrase) {
+        if(phrase.contains("asparagus")){
+           return phrase.replace("asparagus", "****");
+        } else {
+            return phrase;
+        }
+    }
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+
+    public static String bigger(String a, String b){
+        if (a.length() >= b.length()){
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    //local testing of censorAsparagus.
+    public static void main(String[] args) {
+       System.out.println(censorAsparagus("asparagus"));
+       System.out.println(censorAsparagus("apple"));
+       System.out.println(censorAsparagus("my favorite veg is asparagus"));
+       System.out.println(censorAsparagus("my favorite veg is tomatoes"));
+       System.out.println(censorAsparagus("Here is asparagus and there is asparagus"));
+
+    }
 }
